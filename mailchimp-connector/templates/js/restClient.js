fb2f07a12019aec0a691b8eb7d1e31112245f688
Nikos Alveniotis <nikos.alveniotis@prodyna.com>
2022-09-25 19:02:47 +0300

    added environment variables in magnolia restClients and added scheduleCampaign restCall instead of using custom restClient.js

--- mailchimp-connector/templates/js/restClient.js
+++ mailchimp-connector/templates/js/restClient.js
@@ -1,6 +1,6 @@
 var RestClient = function () {
 
-    const API_KEY = "--";
+
     var HttpClients = Java.type("org.apache.http.impl.client.HttpClients");
     var HttpGet = Java.type("org.apache.http.client.methods.HttpGet");
     var HttpPost = Java.type("org.apache.http.client.methods.HttpPost");
@@ -9,6 +9,11 @@ var RestClient = function () {
     var UrlEncodedFormEntity = Java.type("org.apache.http.client.entity.UrlEncodedFormEntity");
     var BasicNameValuePair = Java.type("org.apache.http.message.BasicNameValuePair");
     var EntityUtils = Java.type("org.apache.http.util.EntityUtils");
+    var System = Java.type("java.lang.System");
+
+
+    const API_KEY = System.getenv("MAILCHIMP_BASIC_AUTH");
+    console.log(API_KEY);
 
     this.get = function (url) {
         var httpClient = HttpClients.createDefault();
@@ -35,7 +40,6 @@ var RestClient = function () {
         }
         httpPost.setHeader("Authorization", API_KEY)
 
-        // httpPost.setEntity(new UrlEncodedFormEntity(data));
         response = httpClient.execute(httpPost);
 
         try {
