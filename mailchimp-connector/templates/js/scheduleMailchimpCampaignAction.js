7cf1d646586aaa1fbc59549fed8936b48f23b52a
Nikos Alveniotis <nikos.alveniotis@prodyna.com>
2022-09-26 13:55:45 +0300

    fixed 400 error on schedule_campaign

--- mailchimp-connector/templates/js/scheduleMailchimpCampaignAction.js
+++ mailchimp-connector/templates/js/scheduleMailchimpCampaignAction.js
@@ -12,10 +12,14 @@ var ScheduleMailchimpCampaignAction = function () {
 
         let campaign = propertyUtil.getString(this.content, "id");
         let scheduleAt = propertyUtil.getString(this.content, "schedule_time");
+        let schedule_time_parse = new Date(scheduleAt);
+        var schedule_utc_time = Date.UTC(schedule_time_parse.getUTCFullYear(), schedule_time_parse.getUTCMonth(),
+            schedule_time_parse.getUTCDate(), schedule_time_parse.getUTCHours(),
+            schedule_time_parse.getUTCMinutes(), schedule_time_parse.getUTCSeconds());
 
 
-        let res = restfn.call("mailchimpCampaigns", "scheduleCampaign", {"id": campaign, "schedule_time": scheduleAt});
-        console.log(res);
+        const schedule_time = new Date(schedule_utc_time).toISOString();
+        restfn.call("mailchimpCampaigns", "scheduleCampaign", {"id": campaign, "schedule_time": schedule_time});
         console.log("execute end");
 
     }
